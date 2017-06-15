e084a58f15e65cac620da259471ed83ace195f36
Colin Bookman <bookman@nebuchadnezzar.mtv.corp.google.com>
2017-06-15 00:16:46 -0700

    Adding legal stuffs

--- src/main/java/com/google/cloud/dataflow/teleport/GcsToDatastore.java
+++ src/main/java/com/google/cloud/dataflow/teleport/GcsToDatastore.java
@@ -1,3 +1,16 @@
+/*
+  Copyright 2017 Google Inc.
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+      http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
 package com.google.cloud.dataflow.teleport;
 
 import com.google.cloud.dataflow.teleport.Helpers.JSTransform;
@@ -6,8 +19,6 @@ import com.google.datastore.v1.Entity.Builder;
 import com.google.protobuf.util.JsonFormat;
 import com.google.protobuf.util.JsonFormat.TypeRegistry;
 import java.io.IOException;
-import javax.annotation.Nullable;
-import javax.script.Invocable;
 import javax.script.ScriptException;
 import org.apache.beam.runners.dataflow.DataflowRunner;
 import org.apache.beam.sdk.Pipeline;
